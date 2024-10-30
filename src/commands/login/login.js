c765a7e5d1fa2e670bd5e50b5387636f5fe63eea
A. MULCEY <amaury.mulcey@etudiant.univ-lr.fr>
2024-10-30 11:08:04 +0100

    La commande fonctionne

--- src/commands/login/login.js
+++ src/commands/login/login.js
@@ -1,11 +1,14 @@
+// Importation des modules nécessaires
 const { SlashCommandBuilder } = require('discord.js');
-const ical = require('ical');
 
 const { lruBot } = require('../../dbObjects.js')
 
 module.exports = {
+    // Délai de rechargement de la commande en secondes
     cooldown: 0,
+    // Catégorie de la commande
     category: 'login',
+    // Données et options de la commande
     data: new SlashCommandBuilder()
         .setName('login')
         .setDescription('Permet d\'enregistrer ton calendrier moodle/edt')
@@ -22,55 +25,69 @@ module.exports = {
                 .setDescription('URL de votre calendrier moodle ou nom d\'utilisateur ent')
                 .setRequired(true)),
 
+    /**
+     * Exécute la commande de login pour enregistrer un calendrier Moodle ou EDT.
+     * @param {CommandInteraction} interaction - L'interaction de commande provenant de Discord.
+     * @returns {Promise<void>}
+     */
     async execute(interaction) {
+        // Récupérer le choix de l'utilisateur (0 pour Moodle, 1 pour EDT)
         const choice = interaction.options.getInteger('choix-login');
+        // Récupérer l'argument fourni par l'utilisateur (URL ou identifiant)
         const argument = interaction.options.getString('argument');
 
+        // Différer la réponse pour rendre l'interaction éphémère
         await interaction.deferReply({ ephemeral: true });
 
-        try {
-            if ((await lruBot.findAll({ where: { discordid: interaction.user.id } })).length === 0) {
-                await lruBot.create({
-                    discordid: interaction.user.id,
-                    moodlelink: null,
-                    lruid: null,
-                });
-            }
-        } catch {
-            await interaction.editReply('Something went wrong with adding a tag.');
-        }
+        // Trouver ou créer une entrée dans la table lruBot pour l'utilisateur Discord
+        await lruBot.findOrCreate({
+            where: { discordid: interaction.user.id },
+            defaults: {
+                lruid: null,
+                moodlelink: null,
+            },
+        });
 
+        // Traiter le choix de l'utilisateur
         switch (choice) {
             case 0:
+                // Si l'utilisateur a choisi Moodle
                 try {
+                    // Tenter de récupérer les données depuis l'URL fournie
                     const response = await fetch(argument);
                     if (response.ok) {
+                        // Mettre à jour le lien Moodle dans la base de données
                         await lruBot.update({ moodlelink: argument }, { where: { discordid: interaction.user.id } });
-
-                        await interaction.editReply('Moodle mit à jour.');
-                    }
-                    else {
+                        // Répondre à l'utilisateur que la mise à jour a réussi
+                        await interaction.editReply('Moodle mis à jour.');
+                    } else {
+                        // Si la récupération échoue, lancer une erreur
                         throw new Error(`Failed to fetch data. Response status: ${response.status}`);
                     }
-                }
-                catch (error) {
+                } catch {
+                    // En cas d'erreur, informer l'utilisateur que l'URL Moodle est non valide
                     await interaction.editReply('URL moodle non valide.');
+                    return null;
                 }
                 break;
             case 1:
+                // Si l'utilisateur a choisi EDT
                 try {
-                    const response = await fetch('https://srv.lru.brno.fr/ics/'+argument);
+                    // Tenter de récupérer les données depuis l'URL générée avec l'identifiant fourni
+                    const response = await fetch('https://srv.lru.brno.fr/ics/' + argument);
                     if (response.ok) {
+                        // Mettre à jour l'identifiant EDT dans la base de données
                         await lruBot.update({ lruid: argument }, { where: { discordid: interaction.user.id } });
-
-                        await interaction.editReply('Edt mit à jour.');
-                    }
-                    else {
+                        // Répondre à l'utilisateur que la mise à jour a réussi
+                        await interaction.editReply('Edt mis à jour.');
+                    } else {
+                        // Si la récupération échoue, lancer une erreur
                         throw new Error(`Failed to fetch data. Response status: ${response.status}`);
                     }
-                }
-                catch (error) {
+                } catch {
+                    // En cas d'erreur, informer l'utilisateur que l'identifiant EDT est non valide
                     await interaction.editReply('Identifiant edt non valide.');
+                    return null;
                 }
                 break;
         }
