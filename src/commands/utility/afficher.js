fda2ec9ef5a4efdcf821674bcc92dd04d16e2acb
Linares Julien <julien.linares@etudiant.univ-lr.fr>
2024-11-03 10:41:32 +0100

    Commande afficher sans la duplication

--- src/commands/utility/afficher.js
+++ src/commands/utility/afficher.js
@@ -1,8 +1,6 @@
-const { SlashCommandBuilder, ButtonBuilder, ButtonStyle, ActionRowBuilder, EmbedBuilder, ComponentType} = require('discord.js');
-const ical = require('ical');
-
-const { lruBot } = require('../../dbObjects.js')
-const {getIcsData, getJsonDataFromIcs} = require("../../../project_modules/ics-manager");
+const { SlashCommandBuilder, ButtonBuilder, ButtonStyle, ActionRowBuilder, EmbedBuilder, ComponentType } = require('discord.js');
+const { lruBot } = require('../../dbObjects.js');
+const { getIcsData, getJsonDataFromIcs } = require("../../../project_modules/ics-manager");
 
 module.exports = {
     cooldown: 0,
@@ -20,165 +18,83 @@ module.exports = {
                 )),
 
     async execute(interaction) {
-        let start = Date.now();
+        const start = Date.now();
         const choice = interaction.options.getInteger('choix-login');
 
         await interaction.deferReply({ ephemeral: true });
-        if ((await lruBot.findAll({ where: { discordid: interaction.user.id } })).length === 0) {
-            await interaction.editReply('Veuillez d\'abord vous connecter avec la commande /login.');
-            return null;
-        }
+
+        // Vérifie si l'utilisateur est connecté
         const user = await lruBot.findOne({ where: { discordid: interaction.user.id } });
-
-        switch (choice) {
-            case 0:
-                try {
-                    let moodleIcsData;
-                    try {
-                        moodleIcsData = await getIcsData(user.moodlelink);
-                    } catch (error) {
-                        console.error('Error fetching Moodle data:', error);
-                        return interaction.editReply({ content: 'Impossible de récuperer votre agenda Moodle.' });
-                    }
-
-                    let moodleJsonData = getJsonDataFromIcs(moodleIcsData,5);
-
-                    const previous = new ButtonBuilder()
-                        .setCustomId('previous')
-                        .setLabel('◀️')
-                        .setStyle(ButtonStyle.Primary);
-
-                    const next = new ButtonBuilder()
-                        .setCustomId('next')
-                        .setLabel('▶️')
-                        .setStyle(ButtonStyle.Primary);
-
-                    const row = new ActionRowBuilder().addComponents(previous, next);
-
-                    let curPage = 1;
-
-                    let message = await interaction.editReply({
-                        embeds: [new EmbedBuilder()
-                            .setColor(0x0099ff)
-                            .setTitle(`Voici vos activité Moodle ${interaction.user.displayName} ;)`)
-                            .setDescription(`Page ${curPage}/${Object.keys(moodleJsonData).length}`)
-                            .addFields(getFieldsMoodle(moodleJsonData, curPage))
-                            .setTimestamp()
-                            .setFooter({ text: `Requête réalisé en ${Date.now() - start}ms`, iconURL: 'https://images-ext-1.discordapp.net/external/kMhIi1qtRXShjyfpUaFtpeANp0CUby-IxgrbxN6JUhw/https/www.univ-larochelle.fr/wp-content/uploads/png/logo-universite-de-la-rochelle-2X.png' })
-                        ],
-                        components: [row]
-                    });
-
-                    const collector = message.createMessageComponentCollector({ componentType: ComponentType.Button, time: 60_000 }); // attention : time ne se reset pas à chaque interaction
-
-                    collector.on('collect', async i => {
-                        start = Date.now();
-                        if (i.customId === 'previous') {
-                            curPage = Math.max(curPage - 1, 1);
-                        } else if (i.customId === 'next') {
-                            curPage = Math.min(curPage + 1, Object.keys(moodleJsonData).length);
-                        }
-
-                        await i.update({ embeds: [new EmbedBuilder()
-                                .setColor(0x0099ff)
-                                .setTitle(`Voici vos activité Moodle ${interaction.user.displayName} ;)`)
-                                .setDescription(`Page ${curPage}/${Object.keys(moodleJsonData).length}`)
-                                .addFields(getFieldsMoodle(moodleJsonData, curPage))
-                                .setTimestamp()
-                                .setFooter({ text: `Requête réalisé en ${Date.now() - start}ms`, iconURL: 'https://images-ext-1.discordapp.net/external/kMhIi1qtRXShjyfpUaFtpeANp0CUby-IxgrbxN6JUhw/https/www.univ-larochelle.fr/wp-content/uploads/png/logo-universite-de-la-rochelle-2X.png' })
-                            ],
-                        });
-                    });
-                }
-                catch (error) {
-                    await interaction.editReply('URL moodle non valide.');
-                }
-                break;
-            case 1:
-                const url = 'https://srv.lru.brno.fr/ics/' + user.lruid;
-                let EdtIcsData;
-                try {
-                    EdtIcsData = await getIcsData(url);
-                } catch (error) {
-                    console.error('Error fetching EDT data:', error);
-                    return interaction.editReply({ content: 'Impossible de récuperer votre EDT.' });
-                }
-
-                let EdtJsonData = getJsonDataFromIcs(EdtIcsData,5);
-
-                const previous = new ButtonBuilder()
-                    .setCustomId('previous')
-                    .setLabel('◀️')
-                    .setStyle(ButtonStyle.Primary);
-
-                const next = new ButtonBuilder()
-                    .setCustomId('next')
-                    .setLabel('▶️')
-                    .setStyle(ButtonStyle.Primary);
-
-                const row = new ActionRowBuilder().addComponents(previous, next);
-
-                let curPage = 1;
-
-                let message = await interaction.editReply({
-                    embeds: [new EmbedBuilder()
-                        .setColor(0x0099ff)
-                        .setTitle(`Voici vos prochain cours ${interaction.user.displayName} ;)`)
-                        .setDescription(`Page ${curPage}/${Object.keys(EdtJsonData).length}`)
-                        .addFields(getFieldEDT(EdtJsonData, curPage))
-                        .setTimestamp()
-                        .setFooter({ text: `Requête réalisé en ${Date.now() - start}ms`, iconURL: 'https://images-ext-1.discordapp.net/external/kMhIi1qtRXShjyfpUaFtpeANp0CUby-IxgrbxN6JUhw/https/www.univ-larochelle.fr/wp-content/uploads/png/logo-universite-de-la-rochelle-2X.png' })
-                    ],
-                    components: [row]
-                });
-
-                const collector = message.createMessageComponentCollector({ componentType: ComponentType.Button, time: 60_000 }); // attention : time ne se reset pas à chaque interaction
-
-                collector.on('collect', async i => {
-                    start = Date.now();
-                    if (i.customId === 'previous') {
-                        curPage = Math.max(curPage - 1, 1);
-                    } else if (i.customId === 'next') {
-                        curPage = Math.min(curPage + 1, Object.keys(EdtJsonData).length);
-                    }
-
-                    await i.update({ embeds: [new EmbedBuilder()
-                            .setColor(0x0099ff)
-                            .setTitle(`Voici vos prochain cours ${interaction.user.displayName} ;)`)
-                            .setDescription(`Page ${curPage}/${Object.keys(EdtJsonData).length}`)
-                            .addFields(getFieldEDT(EdtJsonData, curPage))
-                            .setTimestamp()
-                            .setFooter({ text: `Requête réalisé en ${Date.now() - start}ms`, iconURL: 'https://images-ext-1.discordapp.net/external/kMhIi1qtRXShjyfpUaFtpeANp0CUby-IxgrbxN6JUhw/https/www.univ-larochelle.fr/wp-content/uploads/png/logo-universite-de-la-rochelle-2X.png' })
-                        ],
-                    });
-                });
-                break;
+        if (!user) {
+            await interaction.editReply('Veuillez d\'abord vous connecter avec la commande /login.');
+            return;
         }
+
+        // Détermine l'URL du calendrier selon le choix
+        const url = choice === 0 ? user.moodlelink : `https://srv.lru.brno.fr/ics/${user.lruid}`;
+        const title = choice === 0 ? `Voici vos activité Moodle ${interaction.user.displayName} ;)` : `Voici vos prochain cours ${interaction.user.displayName} ;)`;
+        const getFieldFunction = choice === 0 ? getFieldsMoodle : getFieldsEDT;
+
+        // Récupère et convertit les données ICS
+        let calendarData;
+        try {
+            const icsData = await getIcsData(url);
+            calendarData = getJsonDataFromIcs(icsData, 5);
+        } catch (error) {
+            console.error(`Error fetching ${choice === 0 ? 'Moodle' : 'EDT'} data:`, error);
+            await interaction.editReply(`Impossible de récuperer votre ${choice === 0 ? 'agenda Moodle' : 'EDT'}.`);
+            return;
+        }
+
+        // Crée les boutons de pagination
+        const previous = new ButtonBuilder().setCustomId('previous').setLabel('◀️').setStyle(ButtonStyle.Primary);
+        const next = new ButtonBuilder().setCustomId('next').setLabel('▶️').setStyle(ButtonStyle.Primary);
+        const row = new ActionRowBuilder().addComponents(previous, next);
+
+        let curPage = 1;
+
+        // Affiche la première page du calendrier
+        const message = await interaction.editReply({
+            embeds: [createEmbed(title, calendarData, getFieldFunction, curPage, start)],
+            components: [row]
+        });
+
+        // Gestion des interactions des boutons de pagination
+        const collector = message.createMessageComponentCollector({ componentType: ComponentType.Button, time: 60_000 });
+
+        collector.on('collect', async i => {
+            const start = Date.now();
+            curPage = i.customId === 'previous' ? Math.max(curPage - 1, 1) : Math.min(curPage + 1, Object.keys(calendarData).length);
+            await i.update({ embeds: [createEmbed(title, calendarData, getFieldFunction, curPage, start)] });
+        });
     },
 };
 
-function getFieldsMoodle(jsonData, page) {
-    let fields = [];
-
-    for (const [event] in jsonData[page]) {
-        fields.push({
-            name: String(jsonData[page][event].end),
-            value: `**${jsonData[page][event].categories}** : ${jsonData[page][event].summary}`,
-            inline: false
-        });
-    }
-    return fields;
+// Fonction pour créer un embed
+function createEmbed(title, jsonData, getFieldFunction, page, start) {
+    return new EmbedBuilder()
+        .setColor(0x0099ff)
+        .setTitle(title)
+        .setDescription(`Page ${page}/${Object.keys(jsonData).length}`)
+        .addFields(getFieldFunction(jsonData, page))
+        .setTimestamp()
+        .setFooter({ text: `Requête réalisé en ${Date.now() - start}ms`, iconURL: 'https://images-ext-1.discordapp.net/external/kMhIi1qtRXShjyfpUaFtpeANp0CUby-IxgrbxN6JUhw/https/www.univ-larochelle.fr/wp-content/uploads/png/logo-universite-de-la-rochelle-2X.png' });
 }
 
-function getFieldEDT(jsonData, page) {
-    let fields = [];
+// Fonction pour formater les données Moodle
+function getFieldsMoodle(jsonData, page) {
+    return jsonData[page].map(event => ({
+        name: String(event.end),
+        value: `**${event.categories}** : ${event.summary}`,
+        inline: false
+    }));
+}
 
-    for (const [event] in jsonData[page]) {
-        fields.push({
-            name: String(jsonData[page][event].start),
-            value: `${jsonData[page][event].summary}`,
-            inline: false
-        });
-    }
-    return fields;
+// Fonction pour formater les données EDT
+function getFieldsEDT(jsonData, page) {
+    return jsonData[page].map(event => ({
+        name: String(event.start),
+        value: `${event.summary}`,
+        inline: false
+    }));
 }
