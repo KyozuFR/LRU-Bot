ec0fdc9d5151f56ad72f1f7a6998b04b0f385cb3
Linares Julien <julien.linares@etudiant.univ-lr.fr>
2024-11-08 17:02:13 +0100

    Dev en cours

--- src/commands/utility/join.js
+++ src/commands/utility/join.js
@@ -1,5 +1,7 @@
 // Importation des modules nécessaires
 const { ActionRowBuilder, ButtonBuilder, ButtonStyle, SlashCommandBuilder } = require('discord.js');
+const { getJsonDataFromIcs, getIcsData} = require("../../../project_modules/ics-manager");
+const { users } = require('../../dbObjects.js');
 
 module.exports = {
     // Délai de rechargement de la commande en secondes
@@ -19,6 +21,36 @@ module.exports = {
         // Différer la réponse à l'interaction
         await interaction.deferReply({ ephemeral: true });
 
+        const user = await users.findOne({ where: { discordid: interaction.user.id } });
+        if (!user) {
+            await interaction.editReply('Veuillez d\'abord vous connecter avec la commande /login.');
+            return;
+        }
+        getGroupeEtudiant(`https://apps.univ-lr.fr/cgi-bin/WebObjects/ServeurPlanning.woa/wa/ics?login=${user.lruid}`)
         await interaction.editReply(`bibipboop`);
     },
 };
+async function getGroupeEtudiant(url) {
+    let calendarData;
+    try {
+        calendarData = await getIcsData(url);
+    } catch (error) {
+        console.error(`Error fetching ${choice === 0 ? 'Moodle' : 'EDT'} data:`, error);
+        await interaction.editReply(`Impossible de récuperer votre EDT'}.`);
+        return;
+    }
+    //console.log(calendarData);
+    let count = 0;
+    let tab = [];
+    for (const event in calendarData) {
+        //vérifier que le nom de l'événement n'est pas déja dans tab
+        const tmp = calendarData[event].summary.split(";")[2].split("[");
+        if (tab.includes(tmp)) {
+            continue
+        }
+        tab.push(tmp);
+        count++;
+    }
+    console.log(tab);
+
+}
