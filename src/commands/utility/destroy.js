c7efe15d926f7678a21d30ef3e9d06393698b397
A. MULCEY <amaury.mulcey@etudiant.univ-lr.fr>
2024-11-15 18:01:12 +0100

    creation fonction createChannel

---
+++ src/commands/utility/destroy.js
@@ -0,0 +1,38 @@
+// Importation des modules nécessaires
+const { ActionRowBuilder, ButtonBuilder, ButtonStyle, SlashCommandBuilder } = require('discord.js');
+const fs = require('node:fs');
+const path = require('node:path');
+
+module.exports = {
+    // Délai de rechargement de la commande en secondes
+    cooldown: 5,
+    // Catégorie de la commande
+    category: 'utility',
+    // Données et options de la commande
+    data: new SlashCommandBuilder()
+        .setName('destroy')
+        .setDescription('.'),
+
+    /**
+     * Logique d'exécution de la commande.
+     * @param {Interaction} interaction - L'objet interaction de Discord.js
+     */
+    async execute(interaction) {
+        // Différer la réponse à l'interaction
+        await interaction.deferReply({ ephemeral: true });
+
+        await interaction.editReply(`bibipboop`);
+
+        // recupère la liste des salons à destroy
+        const content = fs.readFileSync(path.resolve(__dirname, '../../../reset.txt'), 'utf8').split(";").slice(0, -1);
+
+        for (elmt in content) {
+            const channel = interaction.guild.channels.cache.get(content[elmt]);
+            if (channel) {
+                channel.delete();
+            }
+        }
+
+        fs.writeFile(path.resolve(__dirname, '../../../reset.txt'), "", err => {});
+    },
+};
--- src/commands/utility/join.js
+++ src/commands/utility/join.js
@@ -1,5 +1,7 @@
 // Importation des modules nécessaires
-const { ActionRowBuilder, ButtonBuilder, ButtonStyle, SlashCommandBuilder } = require('discord.js');
+const { ChannelType, SlashCommandBuilder } = require('discord.js');
+const fs = require('node:fs');
+const path = require("node:path");
 
 module.exports = {
     // Délai de rechargement de la commande en secondes
@@ -19,6 +21,24 @@ module.exports = {
         // Différer la réponse à l'interaction
         await interaction.deferReply({ ephemeral: true });
 
+        await createChannel({
+            "test1":"tp1",
+            "test2":"tp1",
+            "test3":"tp2",
+        });
+
         await interaction.editReply(`bibipboop`);
+
+        async function createChannel(listofchannel) {
+            for (elmt in listofchannel) {
+                let new_chanel = await interaction.guild.channels.create({
+                    name: listofchannel[elmt]+" - "+elmt,
+                    type: ChannelType.GuildText,
+                    parent: interaction.guild,
+                });
+
+                fs.writeFile(path.resolve(__dirname, '../../../reset.txt'), new_chanel.id+";", { flag: 'a+' }, err => {});
+            }
+        }
     },
 };
