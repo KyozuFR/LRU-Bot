c022125d2c93587e785966657d276235f453afe1
A. MULCEY <amaury.mulcey@etudiant.univ-lr.fr>
2024-11-16 15:46:26 +0100

    Empeche de créer channel déjà existant

--- src/commands/utility/join.js
+++ src/commands/utility/join.js
@@ -26,36 +26,62 @@ module.exports = {
      * @param {Interaction} interaction - L'objet interaction de Discord.js
      */
     async execute(interaction) {
-        const licence_name = interaction.options.getString('nom-licence');
-        const licence_years = interaction.options.getInteger('annee');
+        const licenceName = interaction.options.getString('nom-licence');
+        const licenceYears = interaction.options.getInteger('annee');
 
         // Différer la réponse à l'interaction
         await interaction.deferReply({ ephemeral: true });
 
-        let new_category = await interaction.guild.channels.create({
-            name: "L"+licence_years+" - "+licence_name,
-            type: ChannelType.GuildCategory,
-            parent: interaction.guild,
-        });
-        fs.writeFile(path.resolve(__dirname, '../../../reset.txt'), new_category.id+";", { flag: 'a+' }, err => {});
+        let newCategory = await createCategory(interaction, "L"+licenceYears+" - "+licenceName)
 
-        await createChannel({
-            "cours1":"tp1",
-            "cours2":"tp1",
-            "cours3":"tp2",
-        });
+        let liste_temporaire = {"cours1":"tp1", "cours2":"tp1",  "cours3":"tp2"};
+        await createChannels(interaction, liste_temporaire, newCategory);
 
         await interaction.editReply(`Groupe assigné`);
-
-        async function createChannel(listofchannel) {
-            for (elmt in listofchannel) {
-                let new_chanel = await interaction.guild.channels.create({
-                    name: listofchannel[elmt]+" - "+elmt,
-                    type: ChannelType.GuildText,
-                    parent: new_category,
-                });
-                fs.writeFile(path.resolve(__dirname, '../../../reset.txt'), new_chanel.id+";", { flag: 'a+' }, err => {});
-            }
-        }
     },
 };
+
+async function createCategory(interaction, categoryName) {
+    let existingCategory = await findChannelFromName(interaction, categoryName, ChannelType.GuildCategory);
+    if (existingCategory) {
+        return existingCategory;
+    }
+
+    let newCategory = await interaction.guild.channels.create({
+        name: categoryName,
+        type: ChannelType.GuildCategory,
+    });
+
+    fs.writeFile(path.resolve(__dirname, '../../../reset.txt'), newCategory.id + ";", { flag: 'a+' }, err => {});
+
+    return newCategory;
+}
+
+async function createChannels(interaction, listOfChannel, categoryParent) {
+    for (let [course, group] of Object.entries(listOfChannel)) {
+        let existingChannel = await findChannelFromName(interaction, group + "-" + course, ChannelType.GuildText, categoryParent);
+        if (!existingChannel) {
+            let newChannel = await interaction.guild.channels.create({
+                name: group + "-" + course,
+                type: ChannelType.GuildText,
+                parent: categoryParent.id,
+            });
+
+            fs.writeFile(path.resolve(__dirname, '../../../reset.txt'), newChannel.id+";", { flag: 'a+' }, err => {});
+        }
+    }
+}
+
+async function findChannelFromName(interaction, name, objectType, categoryParent = interaction.guild) { // à revoir plus tard
+    let guildChannels = interaction.guild.channels.cache;
+    for (let [id, channel] of guildChannels) {
+        if (channel.type === objectType && channel.name.toLowerCase() === name.toLowerCase()) {
+            if (objectType === ChannelType.GuildCategory) { // ceci gère les catégories
+                return channel;
+            } else if (objectType === ChannelType.GuildText && categoryParent.id === channel.parentId) { // ceci ce qui peut être dans des catégories (pour le moment que des salon text)
+                return channel;
+            }
+        }
+    }
+    return false;
+}
