bbafe3dcda033e0b4c6e7cb2be3ae97498decc18
cbillot <corentin.billot@etudiant.univ-lr.fr>
2024-11-20 14:25:50 +0100

    help du join, ping, display, help general (a finir help du login)

--- src/commands/utility/help.js
+++ src/commands/utility/help.js
@@ -36,31 +36,34 @@ module.exports = {
             case 0:
                 const titleLog = "Manuel de la commande /login";
                 const descriptionLog = "Permet de se connecter à l'Université de La Rochelle pour afficher son emploie du temps";
+                const fieldsLog = [{ name: 'Utilisation', value: '/login ‹methode› ‹identifiant›'},
+                    { name: 'Methode', value: 'moodle - ent'},
+                    { name: 'Cooldown', value: '1s', inline: true },
+                    { name: 'Permissions', value: 'Aucune', inline: true }];
                 let curPageLog = 1;
                 const messageLog = await interaction.editReply({
-                    embeds: [createEmbed(titleLog, descriptionLog)],
+                    embeds: [createEmbed(titleLog, descriptionLog, fieldsLog)],
                     components: [row]
                 });
+
                 const collectorLog = messageLog.createMessageComponentCollector({ componentType: ComponentType.Button, time: 60_000 });
                 collectorLog.on('collect', async i => {
                     const start = Date.now();
-                    await i.update({ embeds: [createEmbed(titleLog, descriptionLog )] });
+                    await i.update({ embeds: [createEmbed(titleLog, descriptionLog, fieldsLog)] });
                 });
                 break;
 
 
             case 1:
                 const titleDis = "Manuel de la commande /display";
-                const descriptionDis = "Permet d'afficher ton calendrier moodle/edt";
+                const descriptionDis = "Permet d'afficher son emploie du temps ou ses activité moodle";
+                const fieldsDis = [{ name: 'Utilisation', value: '/display ‹methode›' },
+                    { name: 'Methode', value: 'moodle - edt'},
+                    { name: 'Cooldown', value: '0s', inline: true },
+                    { name: 'Permissions', value: 'Aucune', inline: true }];
                 let curPageDis = 1;
                 const messageDis = await interaction.editReply({
-                    embeds: [createEmbed(titleDis, descriptionDis)],
-                    components: [row]
-                });
-                const collectorDis = messageDis.createMessageComponentCollector({ componentType: ComponentType.Button, time: 60_000 });
-                collectorDis.on('collect', async i => {
-                    const start = Date.now();
-                    await i.update({ embeds: [createEmbed(titleDis, descriptionDis )] });
+                    embeds: [createEmbed(titleDis, descriptionDis, fieldsDis)],
                 });
                 break;
 
@@ -68,60 +71,57 @@ module.exports = {
             case 2:
                 const titleJoin = "Manuel de la commande /join";
                 const descriptionJoin = "Permet de rejoindre tes groupes";
+                const fieldsJoin = [{ name: 'Utilisation', value: '/join ‹licence› ‹année›' },
+                    { name: 'Année', value: 'L1 - L2 - L3'},
+                    { name: 'Cooldown', value: '5s', inline: true },
+                    { name: 'Permissions', value: 'Aucune', inline: true }];
                 let curPageJoin = 1;
                 const messageJoin = await interaction.editReply({
-                        embeds: [createEmbed(titleJoin, descriptionJoin)],
-                    components: [row]
-                });
-                const collectorJoin = messageJoin.createMessageComponentCollector({ componentType: ComponentType.Button, time: 60_000 });
-                collectorJoin.on('collect', async i => {
-                    const start = Date.now();
-                    await i.update({ embeds: [createEmbed(titleJoin, descriptionJoin )] });
+                        embeds: [createEmbed(titleJoin, descriptionJoin, fieldsJoin)]
                 });
                 break;
 
 
             case 3:
                 const titlePing = "Manuel de la commande /ping";
-                const descriptionPing = "Répond pong si le bot est en ligne.";
-                let curPagePing = 1;
+                const descriptionPing = "Permet de verifier si le bot est en ligne et son temps de reaction.\n" +
+                    "Seule la personne ayant lancé la commande peut voir la réponse";
+                const fieldsPing = [{ name: 'Utilisation', value: '/ping' },
+                    { name: 'Cooldown', value: '5s', inline: true },
+                    { name: 'Permissions', value: 'Aucune', inline: true }];
                 const messagePing = await interaction.editReply({
-                    embeds: [createEmbed(titlePing, descriptionPing)],
-                    components: [row]
-                });
-                const collectorPing = messagePing.createMessageComponentCollector({ componentType: ComponentType.Button, time: 60_000 });
-                collectorPing.on('collect', async i => {
-                    const start = Date.now();
-                    await i.update({ embeds: [createEmbed(titlePing, descriptionPing )] });
+                    embeds: [createEmbed(titlePing, descriptionPing, fieldsPing)]
                 });
                 break;
 
 
             default:
                 const titleHelp = "Manuel des commandes";
-                const descriptionHelp = "Liste des commandes disponibles : \n /login \n /display \n /join \n /ping";
+                const descriptionHelp = "Ce bot permet de pouvoir gere differents salon et affecter un utilisateur en fonction de ses groupe de travail a l'université\n" +
+                    "Il permet aussi de pouvoir afficher son emploie du temps et ses activité moodle";
+                const fieldsHelp = [{ name: 'Commandes', value: '/login\n/display\n/join\n/ping' },
+                    { name: 'Utilisation', value: "/help ‹commande›" },
+                    { name: 'Cooldown', value: '1s', inline: true }];
+
                 let curPageHelp = 1;
                 const messageHelp = await interaction.editReply({
-                    embeds: [createEmbed(titleHelp, descriptionHelp)],
-                    components: [row]
-                });
-                const collectorHelp = messageHelp.createMessageComponentCollector({ componentType: ComponentType.Button, time: 60_000 });
-                collectorHelp.on('collect', async i => {
-                    const start = Date.now();
-                    await i.update({ embeds: [createEmbed(titleHelp,descriptionHelp )] });
+                    embeds: [createEmbed(titleHelp, descriptionHelp, fieldsHelp)]
                 });
                 break;
         }
-
     },
 };
 
-function createEmbed(title, description) {
+function createEmbed(title, description, fields) {
     return new EmbedBuilder()
         .setColor(0x0F6EB1)
         .setTitle(title)
         .setDescription(description)
-        .addFields()
+        .addFields(fields)
         .setTimestamp()
         .setFooter({ text: `Requête réalisé`, iconURL: 'https://images-ext-1.discordapp.net/external/kMhIi1qtRXShjyfpUaFtpeANp0CUby-IxgrbxN6JUhw/https/www.univ-larochelle.fr/wp-content/uploads/png/logo-universite-de-la-rochelle-2X.png' });}
 
+function EmbedImage(title, description, fields, image){
+    return createEmbed(title, description, fields)
+        .setImage(image);
+}
