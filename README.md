4a84fe67bcfb4511187209fd07e940151f8ac0a0
cbillot <corentin.billot@etudiant.univ-lr.fr>
2024-11-21 21:58:59 +0100

    ça fonctionne mais bon...

--- README.md
+++ README.md
@@ -1,8 +1,7 @@
 # LRU Bot - Projet 2024-2025 - Cerise Sans Malediction
 
 ## Description
-Bienvenue dans ce projet de **création d'un bot Discord en Js avec base de données**. Ce bot a pour but de pouvoir **attribuer un rôle** à chaque utilisateur lui donnant accès à **différents channels suivent ses groupes au sein de l'université**, mais aussi, l'utilisateur pourra avoir accès à ses **activité à venir sur Moodle**, son **emploi du temps depuis Discord**.
-
+Bienvenue dans ce projet de **création d'un bot Discord en JS avec base de données**. Ce bot a pour objectif de pouvoir **attribuer un rôle à chaque utilisateur**, lui **donnant accès à différents salons en fonction de ses groupes au sein de l'université**. De plus, l'utilisateur pourra **accéder à ses activités à venir sur Moodle** ainsi qu'à **son emploi du temps depuis Discord**.
 ## Visuel
 
 >A venir
@@ -14,14 +13,14 @@ Bienvenue dans ce projet de **création d'un bot Discord en Js avec base de donn
 - S'être créé **un bot Discord** (https://discord.com/developers/docs/intro).
 
 >#### Pour installer le projet, il faut :
--  Tout d'abord, **télécharger notre projet sur GitLab** *si cela n'est pas déjà fait.*
-- Ensuite lier son bot au projet téléchargé dans le fichier .env en suivant la template.
+- Tout d'abord, **télécharger notre projet sur GitLab** *si cela n'est pas déjà fait.*
+- Ensuite, lier son bot au projet téléchargé dans le fichier .env en suivant le modèle..
 - Pour finir, **exécuter les commandes suivantes** dans le terminal à la racine du projet:
   - **npm install** (installation des packages)
   - **node src/app.js** (exécution du bot)
 
 ## Usage
-Pour pouvoir utiliser ce bot, il faudra tout d'abord **effectuer la commande /login** sur le serveur où le bot se trouve pour pouvoir avoir accès à toutes ses fonctionnalités.
+Pour pouvoir utiliser ce bot, il faudra d'abord **effectuer la commande /login** sur le serveur où le bot se trouve pour accéder à toutes ses fonctionnalités.
 
 
 ## Auteurs et mention
@@ -31,9 +30,9 @@ Pour pouvoir utiliser ce bot, il faudra tout d'abord **effectuer la commande /lo
 
 
 ## Crédit 
-Discord.Js
-LRU Brno
+- Discord.Js
+- LRU Brno
 
 
 ## Statut du projet
->A venir
+Le projet est en cours de développement.
--- src/commands/utility/help.js
+++ src/commands/utility/help.js
@@ -1,5 +1,5 @@
 // Importation des modules nécessaires
-const { SlashCommandBuilder, ButtonBuilder, ButtonStyle, ActionRowBuilder, EmbedBuilder, ComponentType } = require('discord.js');
+const { SlashCommandBuilder, ButtonBuilder, ButtonStyle, ActionRowBuilder, EmbedBuilder, ComponentType, AttachmentBuilder} = require('discord.js');
 
 module.exports = {
     // Délai de rechargement de la commande en secondes
@@ -34,29 +34,47 @@ module.exports = {
 
         switch (choice){
             case 0:
-                const titleLog = "Manuel de la commande /login";
+                const titleLog = "Manuel de la commande /login :";
                 const descriptionLog = "Permet de se connecter à l'Université de La Rochelle pour afficher son emploie du temps";
                 const fieldsLog = [{ name: 'Utilisation', value: '/login ‹methode› ‹identifiant›'},
                     { name: 'Methode', value: 'moodle - ent'},
                     { name: 'Cooldown', value: '1s', inline: true },
                     { name: 'Permissions', value: 'Aucune', inline: true }];
+                const fileLog1 = new AttachmentBuilder('../assets/help/identifiantEnt.png');
+                const fileLog2 = new AttachmentBuilder('../assets/help/LienMoodle1.png');
+                const fileLog3 = new AttachmentBuilder('../assets/help/LienMoodle2.png');
+                const fileLog4 = new AttachmentBuilder('../assets/help/LienMoodle3.png');
+                const fileLog5 = new AttachmentBuilder('../assets/help/LienMoodle4.png');
+                const fileLog6 = new AttachmentBuilder('../assets/help/LienMoodle5.png');
+                filesLog = [fileLog1, fileLog2, fileLog3, fileLog4, fileLog5, fileLog6];
+                const imagesLog = ['attachment://identifiantEnt.png', "attachment://LienMoodle1.png",
+                    "attachment://LienMoodle5.png", "attachment://LienMoodle5.png", "attachment://LienMoodle5.png",
+                    "attachment://LienMoodle5.png"];
                 let curPageLog = 1;
+                const titresImageLog = ["Récupération de l'identifiant ENT", "Récupération du lien Moodle - Première étap 1 :",
+                    "Récupération du lien Moodle - Première étap 2 :", "Récupération du lien Moodle - Première étap 3 :",
+                    "Récupération du lien Moodle - Première étap 4 :", "Récupération du lien Moodle - Première étap 5 :"];
                 const messageLog = await interaction.editReply({
-                    embeds: [createEmbed(titleLog, descriptionLog, fieldsLog)],
+                    embeds: [EmbedImage(titleLog, descriptionLog, fieldsLog,titresImageLog[curPageLog-1],
+                        imagesLog[curPageLog-1])], files: [filesLog[curPageLog-1]],
                     components: [row]
                 });
 
                 const collectorLog = messageLog.createMessageComponentCollector({ componentType: ComponentType.Button, time: 60_000 });
                 collectorLog.on('collect', async i => {
                     const start = Date.now();
-                    await i.update({ embeds: [createEmbed(titleLog, descriptionLog, fieldsLog)] });
+                    curPageLog = i.customId === 'previous' ? Math.max(curPageLog - 1, 1) : Math.min(curPageLog + 1,
+                        Object.keys(imagesLog).length);
+                    await i.update({ embeds: [EmbedImage(titleLog, descriptionLog, fieldsLog,titresImageLog[curPageLog-1],
+                            imagesLog[curPageLog-1])],
+                        files: [filesLog[curPageLog-1]] });
                 });
                 break;
 
 
             case 1:
-                const titleDis = "Manuel de la commande /display";
-                const descriptionDis = "Permet d'afficher son emploie du temps ou ses activité moodle";
+                const titleDis = "Manuel de la commande /display :";
+                const descriptionDis = "Permet d'afficher son emploi du temps ou ses activités Moodle";
                 const fieldsDis = [{ name: 'Utilisation', value: '/display ‹methode›' },
                     { name: 'Methode', value: 'moodle - edt'},
                     { name: 'Cooldown', value: '0s', inline: true },
@@ -69,7 +87,7 @@ module.exports = {
 
 
             case 2:
-                const titleJoin = "Manuel de la commande /join";
+                const titleJoin = "Manuel de la commande /join :";
                 const descriptionJoin = "Permet de rejoindre tes groupes";
                 const fieldsJoin = [{ name: 'Utilisation', value: '/join ‹licence› ‹année›' },
                     { name: 'Année', value: 'L1 - L2 - L3'},
@@ -83,9 +101,9 @@ module.exports = {
 
 
             case 3:
-                const titlePing = "Manuel de la commande /ping";
-                const descriptionPing = "Permet de verifier si le bot est en ligne et son temps de reaction.\n" +
-                    "Seule la personne ayant lancé la commande peut voir la réponse";
+                const titlePing = "Manuel de la commande /ping :";
+                const descriptionPing = "Permet de vérifier si le bot est en ligne et son temps de réaction.\n" +
+                    "Seule la personne ayant lancé la commande peut voir la réponse.";
                 const fieldsPing = [{ name: 'Utilisation', value: '/ping' },
                     { name: 'Cooldown', value: '5s', inline: true },
                     { name: 'Permissions', value: 'Aucune', inline: true }];
@@ -96,9 +114,9 @@ module.exports = {
 
 
             default:
-                const titleHelp = "Manuel des commandes";
-                const descriptionHelp = "Ce bot permet de pouvoir gere differents salon et affecter un utilisateur en fonction de ses groupe de travail a l'université\n" +
-                    "Il permet aussi de pouvoir afficher son emploie du temps et ses activité moodle";
+                const titleHelp = "Manuel des commandes :";
+                const descriptionHelp = "Ce bot permet de gérer différents salons et d'affecter un utilisateur en fonction de ses groupes de travail à l'université\n" +
+                    "Il permet aussi d'afficher son emploi du temps et ses activités Moodle.";
                 const fieldsHelp = [{ name: 'Commandes', value: '/login\n/display\n/join\n/ping' },
                     { name: 'Utilisation', value: "/help ‹commande›" },
                     { name: 'Cooldown', value: '1s', inline: true }];
@@ -121,7 +139,8 @@ function createEmbed(title, description, fields) {
         .setTimestamp()
         .setFooter({ text: `Requête réalisé`, iconURL: 'https://images-ext-1.discordapp.net/external/kMhIi1qtRXShjyfpUaFtpeANp0CUby-IxgrbxN6JUhw/https/www.univ-larochelle.fr/wp-content/uploads/png/logo-universite-de-la-rochelle-2X.png' });}
 
-function EmbedImage(title, description, fields, image){
+function EmbedImage(title, description, fields, titreImage, image){
     return createEmbed(title, description, fields)
+        .addFields({name:titreImage, value:"    "})
         .setImage(image);
 }
